9713cf856d2b987b9a1b4525b3f2ef3b439a9c49
Miguel <miguel.zea@tecsup.edu.pe>
2023-12-17 19:17:58 -0500

    prueba

---
+++ src/main/java/com/miempresa/InventBemapApplication.java
@@ -0,0 +1,13 @@
+package com.miempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventBemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventBemapApplication.class, args);
+	}
+
+}
